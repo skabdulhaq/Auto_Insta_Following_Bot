638bd943df471b19a1beec864efc1238ea281d05
sk.abdul haq <75989562+skabdulhaq@users.noreply.github.com>
2021-11-27 11:04:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from instafollowers import InstaFollower
-# Username of insta
-# user = "mebel83229"
-# password of insta
-# pass_ = "skabdulhaq"
+
+
 user_name = input("Username of insta\n")
 password = input("Password of insta\n")
 page = input("Enter page link of instagram page => ")
